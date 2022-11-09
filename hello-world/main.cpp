65d3ce8319a14f38d49c84e545ce63bf0fba786b
leejoonli <git@github.com>
2022-11-08 18:35:51 -0800

    practice with data types

--- hello-world/main.cpp
+++ hello-world/main.cpp
@@ -4,8 +4,15 @@ using namespace std;
 
 int main()
 {
-    char name[] = "lulu";
-    int i = 1;
-    cout << name << endl;
+    char grade = 'A';
+    string name = "lulu";
+    int age = 9;
+    float pi = 3.14;
+    bool answer = false;
+    if (answer) {
+        cout << name << " is " << age << " years old and always gets an " << grade << "." << endl;
+    } else if (!answer) {
+        cout << "no data" << endl;
+    }
     return 0;
 }
