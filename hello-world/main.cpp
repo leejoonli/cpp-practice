799947dae9cfd9c3c8a19745aeebbd314d9513dd
leejoonli <git@github.com>
2022-11-09 19:21:30 -0800

    practice with cin

--- hello-world/main.cpp
+++ hello-world/main.cpp
@@ -9,10 +9,20 @@ int main()
     int age = 9;
     float pi = 3.14;
     bool answer = false;
-    if (answer) {
+    int test = age + int(pi);
+    /*if (answer) {
         cout << name << " is " << age << " years old and always gets an " << grade << "." << endl;
     } else if (!answer) {
-        cout << "no data" << endl;
-    }
+        cout << test << endl;
+    }*/
+    int i;
+    string c;
+    cout << "type in name: " << endl;
+    cin >> c;
+
+    cout << "type in i: " << endl;
+    cin >> i;
+
+    cout << c << " " << i << endl;
     return 0;
 }
