11dc3df41cd0759541fe875e076997186a07773d
leejoonli <git@github.com>
2022-11-10 19:10:57 -0800

    test integer initialization

--- hello-world/main.cpp
+++ hello-world/main.cpp
@@ -4,31 +4,39 @@ using namespace std;
 
 int main()
 {
-    char grade = 'A';
+    /*char grade = 'A';
     string name = "lulu";
     int age = 9;
     float pi = 3.14;
     bool answer = false;
     int test = age + int(pi);
-    /*if (answer) {
+    if (answer) {
         cout << name << " is " << age << " years old and always gets an " << grade << "." << endl;
     } else if (!answer) {
         cout << test << endl;
-    }*/
+    }
     int i;
     string c;
     cout << "type in full name and i: " << endl;
-    /*getline(cin, c);
+    getline(cin, c);
 
     cout << "type in i: " << endl;
-    cin >> c >> i;*/
+    cin >> c >> i;
 
     getline(cin, c);
     cin >> i;
 
     cout << c << " " << i << endl;
 
-    /*cerr << "error" << endl;
+    cerr << "error" << endl;
     clog << "log message" << endl;*/
+    int i(1);
+    int j(2);
+    int k(i+j);
+    int test(1.1);
+    cout << i << endl;
+    cout << j << endl;
+    cout << k << endl;
+    cout << test << endl;
     return 0;
 }
