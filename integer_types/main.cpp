7b809bea7163ba2084517f38bd967c8465b4cde5
leejoonli <git@github.com>
2022-11-17 13:26:29 -0800

    practice integer initialization

--- integer_types/main.cpp
+++ integer_types/main.cpp
@@ -3,6 +3,20 @@
 using namespace std;
 
 int main() {
-    cout << "Hello world!" << endl;
+
+    // braced initializers
+    // variable may contain random garbage value if not initialized
+    int i;
+
+    // automatically initializes to zero
+    int j{};
+
+    // normal initialization
+    int k{1};
+    int l{2};
+
+    // initialization using expression
+    int m{k + l};
+    cout << i << "\n" << j << "\n" << k << "\n" << l << "\n" << m << endl;
     return 0;
 }
