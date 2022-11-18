fb294790933b96b3edec05c4e9aaf7f049409756
leejoonli <git@github.com>
2022-11-18 13:46:49 -0800

    test printing booleans

--- booleans/main.cpp
+++ booleans/main.cpp
@@ -11,5 +11,21 @@ int main() {
     } else {
         cout << "GO" << endl;
     }
+
+    if(green_light) {
+        cout << "Light is green" << endl;
+    } else {
+        cout << "Light is not green" << endl;
+    }
+
+    cout << "size of bool: " << sizeof(bool) << endl;
+    // printing out booleans will be 1 or 0 by default
+    cout << "red light: " << red_light << endl;
+    cout << "green light: " << green_light << endl;
+
+    // if you want to print "true" or "false" you must put in line 27
+    cout << boolalpha;
+    cout << "red light: " << red_light << endl;
+    cout << "green light: " << green_light << endl;
     return 0;
 }
