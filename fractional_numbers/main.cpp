9401bd3d692a0d1d29ab824d9e28876b67c7ded3
leejoonli <git@github.com>
2022-11-17 16:17:26 -0800

    practice infinity and nan

--- fractional_numbers/main.cpp
+++ fractional_numbers/main.cpp
@@ -4,20 +4,6 @@
 using namespace std;
 
 int main() {
-    /*double i{5.6};
-    double j{}; // initialized to 0
-    double k{}; // initialized to 0
-
-    // infinity as said in the c++ course but results in NaN
-    double l{j/k};
-
-    // evaluates to NaN
-    l = j / k;
-
-    cout << sizeof(float) << endl;
-    cout << sizeof(double) << endl;
-    cout << sizeof(long double) << endl;*/
-
     // use suffixes when initializing floating point variables or the default with be double
     float number_1{1.12345678901234567890f}; // precision: 7
     double number_2{1.12345678901234567890}; // precision: 15
@@ -30,9 +16,39 @@ int main() {
     cout << number_3 << endl;*/
 
     // float problems: the precision is usually too limited for a lot of applications
-    double number_4(192400023.0); // error: narrowing conversion
+    /* float number_4(192400023.0f); // error: narrowing conversion
     cout << setprecision(20);
-    cout << number_4 << endl;
+    cout << number_4 << endl;*/
 
+    // scientific notation, can represent really huge or really small numbers
+    double number_5{192400023};
+    double number_6{1.92400023e8};
+    double number_7{1.924e8}; // can ommit the lower 00023 for simplicity if our application allows
+
+    double number_8{0.00000000003498};
+    double number_9{3.498e-11}; // multiply with 10 exp(-11)
+
+    /*cout << setprecision(20);
+    cout << number_5 << endl;
+    cout << number_6 << endl;
+    cout << number_7 << endl;
+    cout << number_8 << endl;
+    cout << number_9 << endl;*/
+
+    // infinity and NaN
+    double number_10{-5.6};
+    double number_11{};
+    double number_12{};
+
+    double number_13{number_10/number_11};
+
+    cout << setprecision(20);
+    cout << number_10 << endl;
+    cout << number_11 << endl;
+    cout << number_12 << endl;
+    cout << number_13 << endl;
+
+    number_13 = number_11 / number_12;
+    cout << number_13 << endl;
     return 0;
 }
