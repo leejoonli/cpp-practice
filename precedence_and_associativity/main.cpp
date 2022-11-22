32c022a81faeef3f6be999c6b850d8f0f66b3402
leejoonli <git@github.com>
2022-11-22 14:29:20 -0800

    test associativity

--- precedence_and_associativity/main.cpp
+++ precedence_and_associativity/main.cpp
@@ -3,6 +3,26 @@
 using namespace std;
 
 int main() {
-    cout << "Hello world!" << endl;
+    // there is a c++ precedence and associativity table https://en.cppreference.com/w/cpp/language/operator_precedence
+    int a {6};
+    int b {3};
+    int c {8};
+    int d {9};
+    int e {3};
+    int f {2};
+    int g {5};
+
+    int i = a + b * c - d / e - f + g;
+            /*6 + 24 - 3 - 2 + 5
+            addition and subtraction has left to right associativity
+            30 - 5 + 5;*/
+    cout << i << endl;
+
+    int j = a / b * c + d - e + f;
+    // multiplication and division also have left to right associativity
+        /*(((a / b) * c) + d - e + f) looks similar to this
+        2 * 8 + 9 - 3 + 2
+        16 + 9 - 3 + 2 = 24*/
+    cout << j << endl;
     return 0;
 }
