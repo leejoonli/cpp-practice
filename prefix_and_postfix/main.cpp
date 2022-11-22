8b53c915b5f4223de06dc49817130e99680d9308
leejoonli <git@github.com>
2022-11-22 15:44:44 -0800

    test prefix and postfix

--- prefix_and_postfix/main.cpp
+++ prefix_and_postfix/main.cpp
@@ -3,6 +3,22 @@
 using namespace std;
 
 int main() {
-    cout << "Hello world!" << endl;
+    int i = 1;
+
+    // increment with postfix
+    cout << "i is incrementing (postfix): " << i++ << endl;
+    cout << "i is now: " << i << endl;
+
+    // decrement with postfix
+    cout << "i is decrementing (postfix): " << i-- << endl;
+    cout << "i is now: " << i << endl;
+
+    // increment with prefix
+    ++i;
+    cout << "i is incrementing (prefix): " << i << endl;
+
+    // decrement with prefix
+    --i;
+    cout << "i is decrementing (prefix): " << i << endl;
     return 0;
 }
