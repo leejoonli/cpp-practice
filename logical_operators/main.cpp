345cebdb047085d9d4e3dfcc1a06f639a7ec009e
leejoonli <git@github.com>
2022-11-23 14:54:33 -0800

    test combined relational and logical operators

--- logical_operators/main.cpp
+++ logical_operators/main.cpp
@@ -7,10 +7,18 @@ int main() {
     bool j = false;
     bool k = true;
 
+    int l = 45;
+    int m = 20;
+    int n = 11;
+
     cout << boolalpha;
     cout << "i: " << i << endl;
     cout << "j: " << j << endl;
     cout << "k: " << k << endl;
+
+    cout << "l: " << l << endl;
+    cout << "m: " << m << endl;
+    cout << "n: " << n << endl;
 
     cout << "i && j: " << (i && j) << endl;
     cout << "i && k: " << (i && k) << endl;
@@ -25,5 +33,8 @@ int main() {
     cout << "!k: " << !k << endl;
 
     cout << "!(i && j) || k: " << (!(i && j) || k) << endl;
+
+    // logical and relational operators combined in expressions
+    cout << "(l > m) && (l > n): " << ((l > m) && (l > n)) << endl;
     return 0;
 }
