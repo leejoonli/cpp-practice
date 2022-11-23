76beb604dc79f842306180f00adcab6fd38c7a12
leejoonli <git@github.com>
2022-11-23 14:49:29 -0800

    test logical operators

--- logical_operators/main.cpp
+++ logical_operators/main.cpp
@@ -3,6 +3,21 @@
 using namespace std;
 
 int main() {
-    cout << "Hello world!" << endl;
+    bool i = true;
+    bool j = false;
+    bool k = true;
+
+    cout << boolalpha;
+    cout << "i: " << i << endl;
+    cout << "j: " << j << endl;
+    cout << "k: " << k << endl;
+
+    cout << "i && j: " << (i && j) << endl;
+    cout << "i && k: " << (i && k) << endl;
+    cout << "i && j && k: " << (i && j && k) << endl;
+
+    cout << "i || j: " << (i || j) << endl;
+    cout << "i || k: " << (i || k) << endl;
+    cout << "i || j || k: " << (i || j || k) << endl;
     return 0;
 }
