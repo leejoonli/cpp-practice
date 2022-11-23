aeabfade18d0836605a42ec71d1d953c35e5236b
leejoonli <git@github.com>
2022-11-23 14:24:01 -0800

    test relational operators

--- relational_operators/main.cpp
+++ relational_operators/main.cpp
@@ -3,6 +3,19 @@
 using namespace std;
 
 int main() {
-    cout << "Hello world!" << endl;
+    int i = 1;
+    int j = 2;
+
+    cout << "i: " << i << "j: " << j << endl;
+
+    cout << boolalpha; // make bool show up as true or false instead of 1 or 0
+
+    // NEED THE PARENTHESIS OR AN ERROR WILL OCCUR, THIS IS BECAUSE THE << PRECEDENCE IS HIGHER THAN COMPARISON OPERATOR PRECEDENCE
+    cout << "i < j: " << (i < j) << endl;
+    cout << "i < j: " << (i <= j) << endl;
+    cout << "i < j: " << (i > j) << endl;
+    cout << "i < j: " << (i >= j) << endl;
+    cout << "i < j: " << (i == j) << endl;
+    cout << "i < j: " << (i != j) << endl;
     return 0;
 }
