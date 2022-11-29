a118dcf594f9404d398a867b8606c361a159665a
leejoonli <git@github.com>
2022-11-29 15:33:54 -0800

    test if statement

--- flow_control/main.cpp
+++ flow_control/main.cpp
@@ -2,8 +2,16 @@
 
 using namespace std;
 
-int main()
-{
-    cout << "Hello world!" << endl;
+int main() {
+    int i = 55;
+    int j = 60;
+    bool temp = i < j;
+    cout << boolalpha << "temp: " << temp << endl;
+    if(temp == true) {
+        cout << i << " is less than " << j << endl;
+    }
+    if(!temp) {
+        cout << i << " is NOT less than " << j << endl;
+    }
     return 0;
 }
