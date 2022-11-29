730297e98b2cb42efae95b617c19be385a51b5d6
leejoonli <git@github.com>
2022-11-29 13:55:23 -0800

    test numeric limits

--- numeric_limits/main.cpp
+++ numeric_limits/main.cpp
@@ -1,9 +1,17 @@
 #include <iostream>
+#include <limits>
 
 using namespace std;
 
-int main()
-{
-    cout << "Hello world!" << endl;
+int main() {
+    // https://en.cppreference.com/w/cpp/types/numeric_limits
+    // you can use min() and max() while typing the data type as a short, long, float double, long double, etc.
+    cout << "the range for short is from " << numeric_limits<short>::min() << " to " << numeric_limits<short>::max() << endl;
+    cout << "the range for short is from " << numeric_limits<long double>::min() << " to " << numeric_limits<long double>::max() << endl;
+
+    // other facilities
+    // is signed or digits
+    cout << "int is signed: " << numeric_limits<int>::is_signed << endl;
+    cout << "int digits: " << numeric_limits<int>::digits << endl;
     return 0;
 }
