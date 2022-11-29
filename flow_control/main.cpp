e05154ee7a58dab8e75f8f3fd00b71e007b54c4c
leejoonli <git@github.com>
2022-11-29 15:45:46 -0800

    test switch

--- flow_control/main.cpp
+++ flow_control/main.cpp
@@ -5,13 +5,61 @@ using namespace std;
 int main() {
     int i = 55;
     int j = 60;
-    bool temp = i < j;
-    cout << boolalpha << "temp: " << temp << endl;
-    if(temp == true) {
+    /*bool temp = i < j;
+    bool test = false;
+    cout << boolalpha << "temp: " << temp << endl;*/
+
+    /*if(temp == true) {
         cout << i << " is less than " << j << endl;
     }
     if(!temp) {
         cout << i << " is NOT less than " << j << endl;
-    }
+    }*/
+
+    /*if(temp) {
+        cout << i << " is less than " << j << endl;
+    } else if (!temp) {
+        cout << i << " is NOT less than " << j << endl;
+    }*/
+
+    /*if(i < j) {
+        cout << i << " is less than " << j << endl;
+    } else {
+        cout << i << " is NOT less than " << j << endl;
+    }*/
+
+    /*if(test) {
+        if(temp) {
+            cout << i << " is less than " << j << endl;
+        } else {
+            cout << i << " is NOT less than " << j << endl;
+        }
+    }*/
+
+    /*if(temp && test) {
+        cout << "true" << endl;
+    } else if (temp || test) {
+        cout << "maybe" << endl;
+    } else {
+        cout << "false" << endl;
+    }*/
+
+    // testing for several different conditions
+    /*switch(i) {
+        case 55: {
+            cout << "i is 55" << endl;
+        }
+        break;
+
+        case 60: {
+            cout << "i is 60" << endl;
+        }
+        break;
+
+        default: {
+            cout << "no case for i determined" << endl;
+        }
+    }*/
+
     return 0;
 }
