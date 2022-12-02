2585858f0307816b616499acf373745a6e17b38e
leejoonli <git@github.com>
2022-12-01 19:08:10 -0800

    test pointers

--- pointers/main.cpp
+++ pointers/main.cpp
@@ -3,6 +3,16 @@
 using namespace std;
 
 int main() {
-    cout << "Hello world!" << endl;
+    int i = 10;
+    double temp = 1.234;
+    // it is better to separate declarations on different lines
+    int* p_i = &i;
+    // int* p_temp = &temp; // can only store the type for which it was declared
+    // another way of declaring and initializing pointers
+    int* p_i_one{&i};
+    // can use this but not recommended
+    int* p_i_two = nullptr;
+    cout << p_i << endl;
+    cout << &i << endl;
     return 0;
 }
