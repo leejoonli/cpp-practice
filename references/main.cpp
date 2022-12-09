d86101110aaa352919710e6065227342e7c5c64d
leejoonli <git@github.com>
2022-12-08 18:45:44 -0800

    test references

--- references/main.cpp
+++ references/main.cpp
@@ -3,6 +3,16 @@
 using namespace std;
 
 int main() {
-    cout << "Hello world!" << endl;
+    int i = 10;
+    int &ref_i = i; // reference (var_alias)
+    cout << i << endl;
+    cout << &i << endl;
+    cout << ref_i << endl; // same value
+    cout << &ref_i << endl; // same address
+    ref_i = 20;
+    cout << i << endl;
+    cout << &i << endl;
+    cout << ref_i << endl; // same value after reassignment
+    cout << &ref_i << endl; // same address after reassignment
     return 0;
 }
